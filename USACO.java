54300ca097be42e57de196acae8c40ebcdc6650d
weichangyao <wyao1@stuy.edu>
2019-03-06 11:44:41 -0500

    added private variables in bronze

---
+++ USACO.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+public class USACO{
+  private int R;
+  private int C;
+  private int E;
+  private int N;
+  public static int bronze(string filename) throws FileNotFoundException{
+    File f = new File(filename);
+  }
+}
